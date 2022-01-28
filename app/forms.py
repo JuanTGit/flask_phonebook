85fbafb53825e9e95e14131ef9c4cd0e6cbc0e2b
Tejeda <tejeda.j086302@stu.sanjac.edu>
2022-01-27 23:50:59 -0600

    Added db and login

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.widgets import TextArea
 
+class PhonebookForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    phonenumber = StringField('Phone Number', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Add Contact')
 
 class RegisterForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    phone = StringField('Phone', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,75 @@
 from app import app
-from flask import render_template, redirect, url_for
-from app.forms import RegisterForm
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from app.forms import RegisterForm, LoginForm, PhonebookForm
+from app.models import User, Phonebook
 
-@app.route('/')
+@app.route ('/')
 def index():
-    my_name = 'Juan'
-    my_city = 'Houston'
-    my_state = 'Texas'
-    return render_template('index.html', name=my_name, city=my_city, state=my_state)
+    return render_template('index.html')
 
 
-
-
-@app.route('/register', methods = ["GET", "POST"])
+@app.route('/register', methods=["GET","POST"])
 def register():
-    form = RegisterForm()
+    form=RegisterForm()
+    if form.validate_on_submit():
+        
+        username=form.username.data
+        password=form.password.data
+
+        user_exists = User.query.filter(User.username == username)
+
+        if user_exists:
+            flash(f"User with username {username} already exists", "danger")
+            return redirect(url_for('register'))
+        
+        User(username=username, password=password)
+        flash("Thank you for registering!", "primary")
+        return redirect(url_for('index'))
+    return render_template('register.html',form=form)
+  
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        
+        username = form.username.data
+        password = form.password.data
+       
+        user = User.query.filter(User.username == username)
+        
+        
+        if not user or not user.check_password(password):
+            
+            flash('That username and/or password is incorrect', 'danger')
+            return redirect(url_for('login'))
+        
+
+        login_user(user)
+        flash('You have succesfully logged in', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/phonebook', methods=['GET', 'POST'])
+@login_required
+def phonebook():
+    form = PhonebookForm()
+    
     if form.validate_on_submit():
         name = form.name.data
-        phone = form.phone.data
+        phonenumber = form.phonenumber.data
+        email = form.email.data
         address = form.address.data
-        print(name, phone, address)
+
+        Phonebook(name=name, phonenumber=phonenumber, email=email, address=address)
         return redirect(url_for('index'))
-    return render_template('register.html', form = form)
+
+    return render_template('phonebook.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You have logged out", "secondary")
+    return redirect(url_for('index'))
